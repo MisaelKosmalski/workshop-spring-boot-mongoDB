72638a048c4f97617dc80c8b531636063ad918f0
Misael Kosmalski <misaelkosmalski@gmail.com>
2023-05-09 01:07:33 -0300

    User update with PUT

--- src/main/java/com/misaexpress/workshopmongo/services/UserService.java
+++ src/main/java/com/misaexpress/workshopmongo/services/UserService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.misaexpress.workshopmongo.domain.User;
@@ -34,6 +33,17 @@ public class UserService {
 	public void delete (String id) {
 			findById(id);
 			repository.deleteById(id);
+	}
+	
+	public User update (User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
 	}
 	
 	public User fromDTO(UserDTO objDto) {
